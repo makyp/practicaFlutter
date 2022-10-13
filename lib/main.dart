f5df363fc5ac04ad902d10897327f875b51754d6
Makyp <maquip02@hotmail.com>
2022-10-12 21:46:09 -0500

    Se agrego un navegationBar con home busqueda y perfil permitiendo la correcta navegacion entre ellos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'description_place.dart';
-import 'review_list.dart';
-import 'header_appbar.dart';
+
+import 'proyecto_trips.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,34 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Practica',
-      theme: ThemeData(
-
-        primarySwatch: Colors.purple,
-      ),
-      home: const MyHomePage(title: 'Share'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  String descriptionDummy ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
   @override
   Widget build(BuildContext context) {
@@ -47,21 +18,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
            primarySwatch: Colors.blue,
     ),
-          home: Scaffold(
-            //body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
-            body: Stack(
-              children: <Widget>[
-                ListView(
-                  children: <Widget>[
-                    DescriptionPlace("Bahamas", 4, descriptionDummy),
-                    ReviewList()
-
-                ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-       )
-    );//MyHomePage(title: 'Flutter Demo Home Page'),
+          home: ProyectoTrips()
+    );
   }
 }
---
+++ lib/proyecto_trips.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'home_trip.dart';
+import 'search_trips.dart';
+import 'profile_trips.dart';
+
+class ProyectoTrips extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+
+    return _ProyectoTrips();
+  }
+
+}
+
+class _ProyectoTrips extends State<ProyectoTrips>{
+  int indexTap=0;//Declaramos e incializamos nuestro indice para la navegacion
+  final List<Widget> widgetsChildren =[// Dentro van la cantidad de vistas que se van mostrando
+    HomeTrip(),
+    SearchTrips(),
+    ProfileTrips(),
+
+  ];
+  void onTapTapped(int index){//Recibe el numero del indice recordemos que va de 0 hasta n-1
+    setState(() {//Llama la funcionalidad del estado del widget
+      indexTap=index;
+    });
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //Para la navegacion
+
+    return Scaffold(
+      body: widgetsChildren[indexTap],//Se colocaran los taps que se desean
+      bottomNavigationBar: Theme(
+        //Personalizacion
+        data: Theme.of(context).copyWith(//Para colorear la barra
+          canvasColor: Colors.white,
+          primaryColor: Colors.purple //Este es el color que queremos para los iconos cuando se seleccionen
+
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: onTapTapped,//Que sucede cuando alguien le de tap
+          currentIndex: indexTap,//Indicar que el indice que se aprege sea el ingresado
+          items: [ //Dentro se colocan los taps que tenga nuestro bottonNavigation
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), //Home es un icono por defecto que nos proporcionan
+                label: (" "),//Titulo de la seccion, si no tiene en blanco
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search), //Home es un icono por defecto que nos proporcionan
+              label: (" "),//Titulo de la seccion, si no tiene en blanco
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person), //Home es un icono por defecto que nos proporcionan
+              label: (" "),//Titulo de la seccion, si no tiene en blanco
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
